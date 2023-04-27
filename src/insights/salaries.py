f083b907024717fdb1e9840b88039e2fbca9c470
caren <carenoliveirasilvapontes@gmail.com>
2023-04-27 18:11:19 -0300

    feat:matches  salay and filter salary

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -31,46 +31,33 @@ def get_min_salary(path: str) -> int:
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
-    """Checks if a given salary is in the salary range of a given job
 
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
+    try:
+        m_salary = job["min_salary"]
+        mx_min_salary = job["max_salary"]
+        if int(m_salary) > int(mx_min_salary):
+            raise ValueError
 
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
+        return int(job["min_salary"]) <= int(salary) <= int(job["max_salary"])
 
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    raise NotImplementedError
+    except TypeError:
+        raise ValueError
+
+    except KeyError:
+        raise ValueError
 
 
 def filter_by_salary_range(
     jobs: List[dict], salary: Union[str, int]
 ) -> List[Dict]:
-    """Filters a list of jobs by salary range
+    filtered = []
 
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
+    for job in jobs:
+        try:
+            match = matches_salary_range(job, salary)
+            if match:
+                filtered.append(job)
+        except ValueError:
+            pass
 
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    raise NotImplementedError
+    return filtered

8c37a44fabcbcc333f6a6831ab5fd8f76f501e35
caren <carenoliveirasilvapontes@gmail.com>
2023-04-27 18:23:33 -0300

    feat:test counter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,8 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    counter = count_ocurrences('data/jobs.csv', 'Python')
+    assert counter == 1639
+    counter_lower = count_ocurrences('data/jobs.csv', 'python')
+    assert counter_lower == 1639

69b375ef9d9925736668a13cf30df145d7e0d12a
caren <carenoliveirasilvapontes@gmail.com>
2023-04-27 14:47:45 -0300

    feat:iniciando o projeto

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -53,3 +53,5 @@ def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
         List of jobs with provided job_type
     """
     raise NotImplementedError
+
+# "iniciando o projeto!"

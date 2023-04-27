ea7a1e89e41c9950ccbc53d5efebc59c51ea0eb6
caren <carenoliveirasilvapontes@gmail.com>
2023-04-27 18:10:54 -0300

    fix: get unique para não aceitar valor vazio

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -4,23 +4,9 @@ from src.insights.jobs import read
 
 def get_unique_industries(path: str) -> List[str]:
     read_csv = read(path)
-    industry_unique = [ind['industry'] for ind in read_csv]
-    return set(industry_unique)
+    industry_u = [ind["industry"] for ind in read_csv if ind['industry'] != '']
+    return set(industry_u)
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    raise NotImplementedError
+    return [job for job in jobs if job["industry"] == industry]

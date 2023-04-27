9b6598de7a7c3d373b62777dfa09acf5e211d366
caren <carenoliveirasilvapontes@gmail.com>
2023-04-27 16:53:26 -0300

    get unique industries

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,22 +1,11 @@
 from typing import List, Dict
+from src.insights.jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+    read_csv = read(path)
+    industry_unique = [ind['industry'] for ind in read_csv]
+    return set(industry_unique)
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:

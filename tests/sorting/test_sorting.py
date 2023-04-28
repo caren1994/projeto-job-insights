6ead186bee6f37aaf3ad8106295ae4cf0af3fc50
caren <carenoliveirasilvapontes@gmail.com>
2023-04-28 12:55:13 -0300

    feat:test sorting

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,21 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    pass
+    jobs = [
+        {"min_salary": 1000, "max_salary": 2100, "date_posted": "2020-08-15"},
+        {"min_salary": 1350, "max_salary": 2500, "date_posted": "2021-08-16"},
+        {"min_salary": 1600, "max_salary": 3700, "date_posted": "2022-08-17"},
+        {"min_salary": 1800, "max_salary": 5000, "date_posted": "2023-08-18"},
+    ]
+
+    salary_min = [jobs[0], jobs[1], jobs[2], jobs[3]]
+    salary_max = [jobs[3], jobs[2], jobs[1], jobs[0]]
+    date_posted = [jobs[3], jobs[2], jobs[1], jobs[0]]
+
+    sort_by(jobs, "min_salary")
+    assert jobs == salary_min
+    sort_by(jobs, "max_salary")
+    assert jobs == salary_max
+    sort_by(jobs, "date_posted")
+    assert jobs == date_posted
